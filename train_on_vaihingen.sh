74fd8ebc04d393d664d6bbf054fa6b41e3585090
Karel Ha <karel.ha@tum.de>
2022-11-30 16:31:05 +0100

    Use 6 object classes in `train_on_vaihingen.sh`
    Signed-off-by: Karel Ha <karel.ha@tum.de>

--- train_on_vaihingen.sh
+++ train_on_vaihingen.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 #!/bin/bash
 
-python train_on_vaihingen.py --amp
+## From dataset description at https://www.isprs.org/education/benchmarks/UrbanSemLab/semantic-labeling.aspx
+# Six categories/classes have been defined:
+#  Impervious surfaces (RGB: 255, 255, 255)
+#  Building (RGB: 0, 0, 255)
+#  Low vegetation (RGB: 0, 255, 255)
+#  Tree (RGB: 0, 255, 0)
+#  Car (RGB: 255, 255, 0)
+#  Clutter/background (RGB: 255, 0, 0)
+python train_on_vaihingen.py --amp --classes 6
