094acf49cdd2d6721792862e669c8b86803f3360
Karel Ha <karel.ha@tum.de>
2022-12-06 15:53:57 +0100

    Rename `a` to `rgb_target`
    Signed-off-by: Karel Ha <karel.ha@tum.de>

--- scripts/rgb_to_categorical_demo_script_03.py
+++ scripts/rgb_to_categorical_demo_script_03.py
@@ -21,12 +21,16 @@ vaihingen_lut = [
 
 
 def rgb_to_onehot(rgb_target=None, color_lut=vaihingen_lut):
-    a = torch.Tensor([1, 2, 2, 3, 4, 4, 4, 5])
+    if rgb_target is None:
+        rgb_target = torch.Tensor([1, 2, 2, 3, 4, 4, 4, 5])
+    print(f"rgb_target: {rgb_target}")
+
     b = torch.Tensor([1, 2, 4])
-    lut_indices = torch.zeros_like(a)
-    print(lut_indices)
+
+    lut_indices = torch.zeros_like(rgb_target)
+    print(f"lut_indices: {lut_indices}")
     for i, e in enumerate(b):
-        lut_indices = lut_indices + (a == e) * (i + 1)
+        lut_indices = lut_indices + (rgb_target == e) * (i + 1)
         print(f"i+1 == {i + 1}, lut_indices: {lut_indices}")
 
 
