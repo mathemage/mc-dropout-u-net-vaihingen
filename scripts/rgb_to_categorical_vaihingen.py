00d334f02ef0139cdff8fdd5b76a59c89203a437
Karel Ha <karel.ha@tum.de>
2022-12-09 18:11:59 +0100

    Copy to `rgb_to_categorical_vaihingen.py`
    Signed-off-by: Karel Ha <karel.ha@tum.de>

--- scripts/rgb_to_categorical_vaihingen.py
+++ scripts/rgb_to_categorical_vaihingen.py
@@ -1,15 +1,6 @@
-# from https://discuss.pytorch.org/t/convert-multi-class-segmentation-mask-into-integer-indexed/110667
+# from https://stackoverflow.com/questions/64300830/pytorch-tensor-get-the-index-of-the-element-with-specific-values
 
 import torch
-
-print(torch.__version__)
-# convert rgb mask to integer class labels
-#   red    =  class-0
-#   green  =  class-1
-#   blue   =  class-2
-#   black  =  class-3
-#   white  =  class-4
-#   (bad)  =  class-5
 
 # Six categories/classes have been defined:
 #
@@ -19,34 +10,71 @@ print(torch.__version__)
 # Tree (RGB: 0, 255, 0)
 # Car (RGB: 255, 255, 0)
 # Clutter/background (RGB: 255, 0, 0)
+vaihingen_lut = torch.tensor([
+    [255, 255, 255],
+    [0, 0, 255],
+    [0, 255, 255],
+    [0, 255, 0],
+    [255, 255, 0],
+    [255, 0, 0]
+])
+vaihingen_lut_dict = dict(enumerate(vaihingen_lut))
 
-# create example mask tensor of shape [nBatch, nRGB = 3, height]
-nBatch = 2
-height = 4
-rgb_mask = 255 * torch.ones((nBatch, 3, height), dtype=torch.int64)
-rgb_mask[0, :, 0] = 0  # black
-rgb_mask[0, [1, 2], 1] = 0  # red
-rgb_mask[0, [0, 2], 2] = 0  # green
-rgb_mask[0, [0, 1], 3] = 0  # blue
-rgb_mask[1, :, 0] = 255  # white
-rgb_mask[1, :, 1] = 0  # black
-rgb_mask[1, [0, 1], 2] = 0  # blue
-rgb_mask[1, [1, 2], 3] = 0  # red
-print(rgb_mask.shape)
-print(rgb_mask)
+UNDEF_CLS = -1
 
-# build lookup table
-lut_dim = torch.tensor(rgb_mask.shape)
-lut_dim[1] = -1
-lut = torch.tensor([3, 0, 1, 5, 2, 5, 5, 4])  # color-class encoding
-lut = lut.unsqueeze(-1).expand(lut_dim.tolist())
 
-# convert rgb colors to 0-7 indices
-label_mask = torch.sign(rgb_mask)
-powers2 = torch.tensor([2]) ** torch.arange(3)
-label_mask = (powers2 * label_mask.transpose(1, -1)).transpose(1, -1).sum(dim=1, keepdim=True)
+def linesep():
+    print("_" * 80)
 
-# index into lut to get integer class labels
-label_mask = torch.gather(lut, 1, label_mask).squeeze()
-print(label_mask.shape)
-print(label_mask)
+
+def rgb_to_onehot(rgb_target=None, color_lut=None):
+    if rgb_target is None:
+        rgb_target = torch.Tensor([1, 2, 2, 3, 4, 4, 4, 5])
+        rgb_target = torch.unsqueeze(rgb_target, dim=-1)
+    print(f"rgb_target: {rgb_target}")
+    print(f"rgb_target.shape: {rgb_target.shape}")
+
+    if color_lut is None:
+        color_lut = torch.Tensor([1, 2, 4])
+    print(f"color_lut: {color_lut}")
+
+    lut_indices = torch.zeros(rgb_target.shape[:-1])
+    lut_indices.fill_(UNDEF_CLS)
+    print(f"lut_indices: {lut_indices}")
+    print(f"lut_indices.shape: {lut_indices.shape}")
+
+    for i, e in enumerate(color_lut):
+        offset = -UNDEF_CLS + i
+        lut_indices += torch.all(rgb_target == e, dim=-1) * offset
+        print(f"\ni == {i}, offset == {offset}, e: {e}")
+        print(f"lut_indices: {lut_indices}")
+
+
+if __name__ == "__main__":
+    print(torch.__version__)
+    print(f"vaihingen_lut_dict: {vaihingen_lut_dict}")
+
+    flipped_vaihingen_lut = torch.flip(vaihingen_lut, dims=[0])
+    print(f"flipped_vaihingen_lut: {flipped_vaihingen_lut}")
+    linesep()
+
+    rgb_to_onehot()
+    linesep()
+
+    sample_target1 = torch.cat((
+        vaihingen_lut,
+        flipped_vaihingen_lut
+    ), dim=0)
+    print(f"sample_target1: {sample_target1}")
+    print(f"sample_target1.size: {sample_target1.size()}")
+    rgb_to_onehot(rgb_target=sample_target1, color_lut=vaihingen_lut)
+    linesep()
+
+    sample_target2 = torch.stack([
+        vaihingen_lut,
+        flipped_vaihingen_lut
+    ])
+    print(f"sample_target2: {sample_target2}")
+    print(f"sample_target2.size: {sample_target2.size()}")
+    rgb_to_onehot(rgb_target=sample_target2, color_lut=vaihingen_lut)
+    linesep()
