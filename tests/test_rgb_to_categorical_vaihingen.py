6bc8eea7fcdd09e7a6187f4cd7d1f88062a1bb9f
Karel Ha <karel.ha@tum.de>
2022-12-13 14:43:13 +0100

    Test concatenated LUT and flipped LUT
    Signed-off-by: Karel Ha <karel.ha@tum.de>

--- tests/test_rgb_to_categorical_vaihingen.py
+++ tests/test_rgb_to_categorical_vaihingen.py
@@ -2,7 +2,7 @@
 
 import torch
 
-from rgb_to_categorical_vaihingen import UNDEF_CLS, vaihingen_lut
+from rgb_to_categorical_vaihingen import UNDEF_CLS, vaihingen_lut, rgb_to_onehot
 
 flipped_vaihingen_lut = torch.flip(vaihingen_lut, dims=[0])
 # print(f"flipped_vaihingen_lut: {flipped_vaihingen_lut}")
@@ -12,14 +12,20 @@ def test_undef_cls():
     assert UNDEF_CLS < 0
 
 
-# def test_cat_lut_flipped():
-#     sample_target1 = torch.cat((
-#         vaihingen_lut,
-#         flipped_vaihingen_lut
-#     ), dim=0)
-#     print(f"sample_target1: {sample_target1}")
-#     print(f"sample_target1.size: {sample_target1.size()}")
-#     rgb_to_onehot(rgb_target=sample_target1, color_lut=vaihingen_lut)
+def test_cat_lut_flipped():
+    sample_target1 = torch.cat((
+        vaihingen_lut,
+        flipped_vaihingen_lut
+    ), dim=0)
+    print(f"sample_target1: {sample_target1}")
+    print(f"sample_target1.size: {sample_target1.size()}")
+
+    onehot = rgb_to_onehot(rgb_target=sample_target1, color_lut=vaihingen_lut)
+    print(f"onehot: {onehot}")
+    print(f"onehot.size: {onehot.size()}")
+
+    print(f"onehot == torch.tensor([0., 1., 2., 3., 4., 5., 5., 4., 3., 2., 1., 0.]): {onehot == torch.tensor([0., 1., 2., 3., 4., 5., 5., 4., 3., 2., 1., 0.])}")
+    assert torch.all(onehot == torch.tensor([0., 1., 2., 3., 4., 5., 5., 4., 3., 2., 1., 0.]))
 
 # if __name__ == "__main__":
 #
