1753810248412a295079caf5bcf66d9662bb6c02
Karel Ha <karel.ha@tum.de>
2022-12-13 14:46:52 +0100

    Extract variable `expected_onehot`
    Signed-off-by: Karel Ha <karel.ha@tum.de>

--- tests/test_rgb_to_categorical_vaihingen.py
+++ tests/test_rgb_to_categorical_vaihingen.py
@@ -5,6 +5,8 @@ import torch
 from rgb_to_categorical_vaihingen import UNDEF_CLS, vaihingen_lut, rgb_to_onehot
 
 flipped_vaihingen_lut = torch.flip(vaihingen_lut, dims=[0])
+
+
 # print(f"flipped_vaihingen_lut: {flipped_vaihingen_lut}")
 
 
@@ -24,17 +26,25 @@ def test_cat_lut_flipped():
     print(f"onehot: {onehot}")
     print(f"onehot.size: {onehot.size()}")
 
-    print(f"onehot == torch.tensor([0., 1., 2., 3., 4., 5., 5., 4., 3., 2., 1., 0.]): {onehot == torch.tensor([0., 1., 2., 3., 4., 5., 5., 4., 3., 2., 1., 0.])}")
-    assert torch.all(onehot == torch.tensor([0., 1., 2., 3., 4., 5., 5., 4., 3., 2., 1., 0.]))
+    expected_onehot = torch.tensor([0., 1., 2., 3., 4., 5., 5., 4., 3., 2., 1., 0.])
+    print(f"expected_onehot): {expected_onehot}")
+    print(f"onehot == expected_onehot): {onehot == expected_onehot}")
+    assert torch.all(onehot == expected_onehot)
 
-# if __name__ == "__main__":
-#
-#     rgb_to_onehot()
-#
+
+# def test_stack_lut_flipped():
 #     sample_target2 = torch.stack([
 #         vaihingen_lut,
 #         flipped_vaihingen_lut
 #     ])
 #     print(f"sample_target2: {sample_target2}")
 #     print(f"sample_target2.size: {sample_target2.size()}")
-#     rgb_to_onehot(rgb_target=sample_target2, color_lut=vaihingen_lut)
+#
+#     onehot = rgb_to_onehot(rgb_target=sample_target2, color_lut=vaihingen_lut)
+#     print(f"onehot: {onehot}")
+#     print(f"onehot.size: {onehot.size()}")
+#
+#     print(
+#         f"onehot == torch.tensor([0., 1., 2., 3., 4., 5., 5., 4., 3., 2., 1., 0.]): {onehot == torch.tensor([0., 1., 2., 3., 4., 5., 5., 4., 3., 2., 1., 0.])}")
+#     assert torch.all(onehot == torch.tensor([0., 1., 2., 3., 4., 5., 5., 4., 3., 2., 1., 0.]))
+#
