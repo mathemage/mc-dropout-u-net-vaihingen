55cdfad7f120988d08e72f2d4a51943f7c4e7015
Karel Ha <karel.ha@tum.de>
2022-12-13 14:50:04 +0100

    Test stacked LUT and flipped LUT
    Signed-off-by: Karel Ha <karel.ha@tum.de>

--- tests/test_rgb_to_categorical_vaihingen.py
+++ tests/test_rgb_to_categorical_vaihingen.py
@@ -32,19 +32,22 @@ def test_cat_lut_flipped():
     assert torch.equal(onehot, expected_onehot)
 
 
-# def test_stack_lut_flipped():
-#     sample_target2 = torch.stack([
-#         vaihingen_lut,
-#         flipped_vaihingen_lut
-#     ])
-#     print(f"sample_target2: {sample_target2}")
-#     print(f"sample_target2.size: {sample_target2.size()}")
-#
-#     onehot = rgb_to_onehot(rgb_target=sample_target2, color_lut=vaihingen_lut)
-#     print(f"onehot: {onehot}")
-#     print(f"onehot.size: {onehot.size()}")
-#
-#     print(
-#         f"onehot == torch.tensor([0., 1., 2., 3., 4., 5., 5., 4., 3., 2., 1., 0.]): {onehot == torch.tensor([0., 1., 2., 3., 4., 5., 5., 4., 3., 2., 1., 0.])}")
-#     assert torch.all(onehot == torch.tensor([0., 1., 2., 3., 4., 5., 5., 4., 3., 2., 1., 0.]))
-#
+def test_stack_lut_flipped():
+    sample_target2 = torch.stack([
+        vaihingen_lut,
+        flipped_vaihingen_lut
+    ])
+    print(f"sample_target2: {sample_target2}")
+    print(f"sample_target2.size: {sample_target2.size()}")
+
+    onehot = rgb_to_onehot(rgb_target=sample_target2, color_lut=vaihingen_lut)
+    print(f"onehot: {onehot}")
+    print(f"onehot.size: {onehot.size()}")
+
+    expected_onehot = torch.tensor([
+        [0., 1., 2., 3., 4., 5.],
+        [5., 4., 3., 2., 1., 0.]
+    ])
+    print(f"expected_onehot): {expected_onehot}")
+    print(f"onehot == expected_onehot): {onehot == expected_onehot}")
+    assert torch.equal(onehot, expected_onehot)
