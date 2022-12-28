fd20e498ad04373e01973ac4e6c594188d42f29f
Karel Ha <karel.ha@tum.de>
2022-12-28 16:46:25 +0100

    Continue transforming test_on_vaihingen.py for test set
    Signed-off-by: Karel Ha <karel.ha@tum.de>

--- test_on_vaihingen.py
+++ test_on_vaihingen.py
@@ -41,6 +41,7 @@ def test_net(net,
     # 2. Create data loaders
     loader_args = dict(batch_size=batch_size, num_workers=4, pin_memory=True)
     test_loader = DataLoader(test_set, shuffle=False, **loader_args)
+    val_loader = DataLoader(test_set, shuffle=False, **loader_args)
 
     # (Initialize logging)
     experiment = wandb.init(project='U-Net', resume='allow', anonymous='must')
@@ -55,13 +56,11 @@ def test_net(net,
     ''')
 
     # 4. Set up the optimizer, the loss, the learning rate scheduler and the loss scaling for AMP
-    grad_scaler = torch.cuda.amp.GradScaler(enabled=amp)
     criterion = nn.CrossEntropyLoss()
     global_step = 0
 
     # 5. Begin training
     for epoch in range(1, epochs + 1):
-        net.train()
         epoch_loss = 0
         with tqdm(total=n_test, desc=f'Epoch {epoch}/{epochs}', unit='img') as pbar:
             for batch in test_loader:
@@ -84,11 +83,6 @@ def test_net(net,
                                        F.one_hot(true_masks, net.n_classes).permute(0, 3, 1, 2).float(),
                                        multiclass=True)
 
-                # optimizer.zero_grad(set_to_none=True)
-                # grad_scaler.scale(loss).backward()
-                # grad_scaler.step(optimizer)
-                # grad_scaler.update()
-
                 pbar.update(images.shape[0])
                 global_step += 1
                 epoch_loss += loss.item()
@@ -109,12 +103,10 @@ def test_net(net,
                             histograms['Weights/' + tag] = wandb.Histogram(value.data.cpu())
                             histograms['Gradients/' + tag] = wandb.Histogram(value.grad.data.cpu())
 
-                        val_score = evaluate(net, val_loader, device)   # TODO implement evaluate() over test set
-                        scheduler.step(val_score)
+                        val_score = evaluate(net, val_loader, device)  # TODO replace val_loader
 
                         logging.info('Validation Dice score: {}'.format(val_score))
                         experiment.log({
-                            'learning rate': optimizer.param_groups[0]['lr'],
                             'validation Dice': val_score,
                             'images': wandb.Image(images[0].cpu()),
                             'masks': {
@@ -135,12 +127,8 @@ def get_args():
     parser = argparse.ArgumentParser(description='Train the UNet on images and target masks')
     parser.add_argument('--epochs', '-e', metavar='E', type=int, default=5, help='Number of epochs')
     parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=1, help='Batch size')
-    parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=1e-5,
-                        help='Learning rate', dest='lr')
     parser.add_argument('--load', '-f', type=str, default=False, help='Load model from a .pth file')
     parser.add_argument('--scale', '-s', type=float, default=0.5, help='Downscaling factor of the images')
-    parser.add_argument('--validation', '-v', dest='val', type=float, default=10.0,
-                        help='Percent of the data that is used as validation (0-100)')
     parser.add_argument('--amp', action='store_true', default=False, help='Use mixed precision')
     parser.add_argument('--bilinear', action='store_true', default=False, help='Use bilinear upsampling')
     parser.add_argument('--classes', '-c', type=int, default=2, help='Number of classes')
@@ -174,10 +162,8 @@ if __name__ == '__main__':
         test_net(net=net,
                  epochs=args.epochs,
                  batch_size=args.batch_size,
-                 learning_rate=args.lr,
                  device=device,
                  img_scale=args.scale,
-                 val_percent=args.val / 100,
                  amp=args.amp)
     except KeyboardInterrupt:
         torch.save(net.state_dict(), 'INTERRUPTED.pth')
