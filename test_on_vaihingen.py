6497d7e60cd8e7cf9a6cbb7c11428bdbda8753b1
Karel Ha <karel.ha@tum.de>
2023-01-25 16:53:23 +0100

    Change argparse description
    Signed-off-by: Karel Ha <karel.ha@tum.de>

--- test_on_vaihingen.py
+++ test_on_vaihingen.py
@@ -124,7 +124,7 @@ def test_net(net,
 
 
 def get_args():
-    parser = argparse.ArgumentParser(description='Train the UNet on images and target masks')
+    parser = argparse.ArgumentParser(description='Test the UNet on images and target masks')
     parser.add_argument('--epochs', '-e', metavar='E', type=int, default=5, help='Number of epochs')
     parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=1, help='Batch size')
     parser.add_argument('--load', '-f', type=str, default=False, help='Load model from a .pth file')
