3ae95e82d1bcb1b4984731b8224588527ed0d816
Karel Ha <karel.ha@tum.de>
2023-01-26 16:05:21 +0100

    Comment out training and use only testing
    Signed-off-by: Karel Ha <karel.ha@tum.de>

--- test_on_vaihingen.py
+++ test_on_vaihingen.py
@@ -43,8 +43,8 @@ def test_net(net,
 
     # 2. Create data loaders
     loader_args = dict(batch_size=batch_size, num_workers=4, pin_memory=True)
-    test_loader = DataLoader(test_set, shuffle=True, **loader_args)
-    val_loader = DataLoader(test_set, shuffle=False, **loader_args)
+    test_loader = DataLoader(test_set, shuffle=False, **loader_args)
+    # val_loader = DataLoader(test_set, shuffle=False, **loader_args)
 
     # (Initialize logging)
     experiment = wandb.init(project='U-Net', resume='allow', anonymous='must')
@@ -59,82 +59,95 @@ def test_net(net,
     ''')
 
     # 4. Set up the optimizer, the loss, the learning rate scheduler and the loss scaling for AMP
-    optimizer = optim.RMSprop(net.parameters(), lr=learning_rate, weight_decay=1e-8, momentum=0.9)
-    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=2)  # goal: maximize Dice score
-    grad_scaler = torch.cuda.amp.GradScaler(enabled=amp)
-    criterion = nn.CrossEntropyLoss()
-    global_step = 0
+    # optimizer = optim.RMSprop(net.parameters(), lr=learning_rate, weight_decay=1e-8, momentum=0.9)
+    # scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=2)  # goal: maximize Dice score
+    # grad_scaler = torch.cuda.amp.GradScaler(enabled=amp)
+    # criterion = nn.CrossEntropyLoss()
+    # global_step = 0
 
     # 5. Begin training
-    for epoch in range(1, epochs + 1):
-        net.train()
-        epoch_loss = 0
-        with tqdm(total=n_test, desc=f'Epoch {epoch}/{epochs}', unit='img') as pbar:
-            for batch in test_loader:
-                images = batch['image']
-                true_masks = batch['mask']
-                true_masks = rgb_to_onehot(rgb_target=true_masks, quiet=True)
+    # for epoch in range(1, epochs + 1):
+    #     net.train()
+    #     epoch_loss = 0
+    #     with tqdm(total=n_test, desc=f'Epoch {epoch}/{epochs}', unit='img') as pbar:
+    #         for batch in test_loader:
+    #             images = batch['image']
+    #             true_masks = batch['mask']
+    #             true_masks = rgb_to_onehot(rgb_target=true_masks, quiet=True)
+    #
+    #             assert images.shape[1] == net.n_channels, \
+    #                 f'Network has been defined with {net.n_channels} input channels, ' \
+    #                 f'but loaded images have {images.shape[1]} channels. Please check that ' \
+    #                 'the images are loaded correctly.'
+    #
+    #             images = images.to(device=device, dtype=torch.float32)
+    #             true_masks = true_masks.to(device=device, dtype=torch.long)
+    #
+    #             with torch.cuda.amp.autocast(enabled=amp):
+    #                 masks_pred = net(images)
+    #                 loss = criterion(masks_pred, true_masks) \
+    #                        + dice_loss(F.softmax(masks_pred, dim=1).float(),
+    #                                    F.one_hot(true_masks, net.n_classes).permute(0, 3, 1, 2).float(),
+    #                                    multiclass=True)
+    #
+    #             optimizer.zero_grad(set_to_none=True)
+    #             grad_scaler.scale(loss).backward()
+    #             grad_scaler.step(optimizer)
+    #             grad_scaler.update()
+    #
+    #             pbar.update(images.shape[0])
+    #             global_step += 1
+    #             epoch_loss += loss.item()
+    #             experiment.log({
+    #                 'test loss': loss.item(),
+    #                 'step': global_step,
+    #                 'epoch': epoch
+    #             })
+    #             pbar.set_postfix(**{'loss (batch)': loss.item()})
+    #
+    #             # Evaluation round
+    #             division_step = (n_test // (10 * batch_size))
+    #             if division_step > 0:
+    #                 if global_step % division_step == 0:
+    #                     histograms = {}
+    #                     for tag, value in net.named_parameters():
+    #                         tag = tag.replace('/', '.')
+    #                         histograms['Weights/' + tag] = wandb.Histogram(value.data.cpu())
+    #                         histograms['Gradients/' + tag] = wandb.Histogram(value.grad.data.cpu())
+    #
+    #                     test_score = evaluate(net, val_loader, device)
+    #
+    #                     logging.info('Validation Dice score: {}'.format(test_score))
+    #                     experiment.log({
+    #                         'validation Dice': test_score,
+    #                         'images': wandb.Image(images[0].cpu()),
+    #                         'masks': {
+    #                             'true': wandb.Image(true_masks[0].float().cpu()),
+    #                             'pred': wandb.Image(masks_pred.argmax(dim=1)[0].float().cpu()),
+    #                         },
+    #                         'step': global_step,
+    #                         'epoch': epoch,
+    #                         **histograms
+    #                     })
+    #
+    #             # optimize memory by deallocating on CUDA
+    #             del true_masks
+    #             del images
+    #             torch.cuda.empty_cache()
 
-                assert images.shape[1] == net.n_channels, \
-                    f'Network has been defined with {net.n_channels} input channels, ' \
-                    f'but loaded images have {images.shape[1]} channels. Please check that ' \
-                    'the images are loaded correctly.'
+    # 6. Begin testing
+    histograms = {}
+    for tag, value in net.named_parameters():
+        tag = tag.replace('/', '.')
+        histograms['Weights/' + tag] = wandb.Histogram(value.data.cpu())
 
-                images = images.to(device=device, dtype=torch.float32)
-                true_masks = true_masks.to(device=device, dtype=torch.long)
+    test_score = evaluate(net, test_loader, device)
 
-                with torch.cuda.amp.autocast(enabled=amp):
-                    masks_pred = net(images)
-                    loss = criterion(masks_pred, true_masks) \
-                           + dice_loss(F.softmax(masks_pred, dim=1).float(),
-                                       F.one_hot(true_masks, net.n_classes).permute(0, 3, 1, 2).float(),
-                                       multiclass=True)
-
-                optimizer.zero_grad(set_to_none=True)
-                grad_scaler.scale(loss).backward()
-                grad_scaler.step(optimizer)
-                grad_scaler.update()
-
-                pbar.update(images.shape[0])
-                global_step += 1
-                epoch_loss += loss.item()
-                experiment.log({
-                    'test loss': loss.item(),
-                    'step': global_step,
-                    'epoch': epoch
-                })
-                pbar.set_postfix(**{'loss (batch)': loss.item()})
-
-                # Evaluation round
-                # TODO find out why this needs to be here
-                division_step = (n_test // (10 * batch_size))
-                if division_step > 0:
-                    if global_step % division_step == 0:
-                        histograms = {}
-                        for tag, value in net.named_parameters():
-                            tag = tag.replace('/', '.')
-                            histograms['Weights/' + tag] = wandb.Histogram(value.data.cpu())
-                            histograms['Gradients/' + tag] = wandb.Histogram(value.grad.data.cpu())
-
-                        val_score = evaluate(net, val_loader, device)  # TODO replace val_loader
-
-                        logging.info('Validation Dice score: {}'.format(val_score))
-                        experiment.log({
-                            'validation Dice': val_score,
-                            'images': wandb.Image(images[0].cpu()),
-                            'masks': {
-                                'true': wandb.Image(true_masks[0].float().cpu()),
-                                'pred': wandb.Image(masks_pred.argmax(dim=1)[0].float().cpu()),
-                            },
-                            'step': global_step,
-                            'epoch': epoch,
-                            **histograms
-                        })
-
-                # optimize memory by deallocating on CUDA
-                del true_masks
-                del images
-                torch.cuda.empty_cache()
+    logging.info('Test Dice score: {}'.format(test_score))
+    experiment.log({
+        'test Dice': test_score,
+        **histograms
+    })
 
 
 def get_args():
