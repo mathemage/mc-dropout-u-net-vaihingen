9b7b569afa8faea2f566f52d95cfa073da3bd6f5
Karel Ha <karel.ha@tum.de>
2023-01-27 16:54:50 +0100

    Default to 6 classes for Vaihingen
    Signed-off-by: Karel Ha <karel.ha@tum.de>

--- test_on_vaihingen.py
+++ test_on_vaihingen.py
@@ -77,7 +77,7 @@ def get_args():
     parser.add_argument('--scale', '-s', type=float, default=0.5, help='Downscaling factor of the images')
     parser.add_argument('--amp', action='store_true', default=False, help='Use mixed precision')
     parser.add_argument('--bilinear', action='store_true', default=False, help='Use bilinear upsampling')
-    parser.add_argument('--classes', '-c', type=int, default=2, help='Number of classes')
+    parser.add_argument('--classes', '-c', type=int, default=6, help='Number of classes')
 
     return parser.parse_args()
 
