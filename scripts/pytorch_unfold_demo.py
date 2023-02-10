c81c3543706ba1b4242c85e4d0e33340c5a3708e
Karel Ha <karel.ha@tum.de>
2023-02-10 16:35:49 +0100

    Include demo of nn.Unfold from PyTorch docs
    Signed-off-by: Karel Ha <karel.ha@tum.de>

--- scripts/pytorch_unfold_demo.py
+++ scripts/pytorch_unfold_demo.py
@@ -17,6 +17,17 @@ x_3d = torch.arange(24.)
 x_3d = torch.reshape(x_3d, (2, 4, 3))
 logging.info(f"x_3d: {x_3d}")
 # logging.info(f"x_3d.unfold(1, 2, 1):\n {x_3d.unfold(dimension=1, size=2, step=1)}")
+
+# From https://pytorch.org/docs/stable/generated/torch.nn.Unfold.html
+unfold = nn.Unfold(kernel_size=(2, 3))
+inp = torch.randn(2, 5, 3, 4)
+logging.info(f"input: {inp}")
+output = unfold(inp)
+logging.info(f"output: {output}")
+# each patch contains 30 values (2x3=6 vectors, each of 5 channels)
+# 4 blocks (2x3 kernels) in total in the 3x4 input
+logging.info(f"output.size(): {output.size()}")  # TODO understand
+
 # From https://pytorch.org/docs/stable/generated/torch.nn.Unfold.html
 unfold_3d = nn.Unfold(kernel_size=(2, 2))(x_3d)
 logging.info(f"unfold_3d: {unfold_3d}")  # TODO check here
