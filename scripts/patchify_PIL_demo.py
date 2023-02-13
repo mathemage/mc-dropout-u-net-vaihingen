7ec34f63bd351b76d29ae64e5b224527b4dfdbbf
Karel Ha <karel.ha@tum.de>
2023-02-13 16:33:39 +0100

    Split patchify_demo.py into 2 demo files
    Signed-off-by: Karel Ha <karel.ha@tum.de>

--- scripts/patchify_PIL_demo.py
+++ scripts/patchify_PIL_demo.py
@@ -4,21 +4,9 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
 
-# From https://github.com/dovahcrow/patchify.py#2d-image-patchify-and-merge
-image = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
-logging.info(f"image: {image}")
-patches = patchify(image, (2, 2), step=1)  # split image into 2*3 small 2*2 patches.
-logging.info(f"patches: {patches}")
-
-assert patches.shape == (2, 3, 2, 2)
-
-reconstructed_image = unpatchify(patches, image.shape)
-assert (reconstructed_image == image).all()
-
 # From https://levelup.gitconnected.com/how-to-split-an-image-into-patches-with-python-e1cf42cf4f77
 from PIL import Image
 
-# root_dir = "./data/vaihingen"
 root_dir = "../data/vaihingen"
 img_dir = f"{root_dir}/imgs"
 import os   # TODO remove
