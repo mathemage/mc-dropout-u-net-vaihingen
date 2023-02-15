e97e43a009df19029153d23efdcbf5e8f07c4880
Karel Ha <karel.ha@tum.de>
2023-02-15 16:21:30 +0100

    Clean up and rearrange patchify_dataset.py
    Signed-off-by: Karel Ha <karel.ha@tum.de>

--- patchify_dataset.py
+++ patchify_dataset.py
@@ -9,33 +9,33 @@ logging_level = logging.INFO
 # logging_level = logging.DEBUG
 logging.basicConfig(level=logging_level, format='[%(levelname)s] %(message)s')
 
-# From https://levelup.gitconnected.com/how-to-split-an-image-into-patches-with-python-e1cf42cf4f77
 root_dir = "./data/vaihingen"
 img_dir = f"{root_dir}/imgs"
-files = [f for f in os.listdir('.') if os.path.isfile(f)]
-logging.debug(f"ls: {files}")
-
-input_file = f"{img_dir}/top_mosaic_09cm_area1.tif"  # ./data/vaihingen/imgs/top_mosaic_09cm_area1.tif
-
-input_image = Image.open(input_file)
-input_image = np.asarray(input_image)
-logging.info(f"image.shape: {input_image.shape}")
 
 patch_size = 128
 channels = 3
-patches = patchify(input_image, (patch_size, patch_size, channels), step=patch_size)
-logging.info(f"patches.shape: {patches.shape}")
 
 output_directory = f"{img_dir}/patches_{patch_size}x{patch_size}x{channels}"
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
-logging.debug(f"output_directory: {output_directory}")
+logging.info(f"output_directory: {output_directory}")
+
+files = [file for file in os.listdir('.') if os.path.isfile(file)]
+logging.debug(f"ls .: {files}")
+
+input_file = f"{img_dir}/top_mosaic_09cm_area1.tif"  # ./data/vaihingen/imgs/top_mosaic_09cm_area1.tif
+input_image = Image.open(input_file)
+input_image = np.asarray(input_image)
+logging.debug(f"image.shape: {input_image.shape}")
+
+patches = patchify(input_image, (patch_size, patch_size, channels), step=patch_size)
+logging.debug(f"patches.shape: {patches.shape}")
 
 for i in range(patches.shape[0]):
     for j in range(patches.shape[1]):
-        output_file = f"{output_directory}/top_mosaic_09cm_area1_patch_{i}_{j}.tif"
         patch = patches[i, j, 0]
         patch = Image.fromarray(patch)
-        num = i * patches.shape[1] + j
+
+        output_file = f"{output_directory}/top_mosaic_09cm_area1_patch_{i}_{j}.tif"
         patch.save(output_file)
         logging.info(f"Patch {output_file} saved.")
