32ec7fc819ba4cf52a0767ad880881a0e53b8f21
Karel Ha <karel.ha@tum.de>
2023-02-16 15:48:53 +0100

    Iterate over all input images to patchify
    Signed-off-by: Karel Ha <karel.ha@tum.de>

--- patchify_dataset.py
+++ patchify_dataset.py
@@ -27,21 +27,26 @@ allowed_extensions = ('.tif', '.tiff')
 input_files = [file for file in input_files if file.endswith(allowed_extensions)]
 logging.info(f"input_files: {' '.join(input_files)}")
 logging.info(f"len(input_files): {len(input_files)}")
-exit(1)
 
-input_file = f"{img_dir}/top_mosaic_09cm_area1.tif"  # ./data/vaihingen/imgs/top_mosaic_09cm_area1.tif
-input_image = Image.open(input_file)
-input_image = np.asarray(input_image)
-logging.debug(f"image.shape: {input_image.shape}")
+# input_file = f"{img_dir}/top_mosaic_09cm_area1.tif"  # ./data/vaihingen/imgs/top_mosaic_09cm_area1.tif
+for input_file in input_files:
+    input_path = f"{img_dir}/{input_file}"
+    logging.info(f"input_path: {input_path}")
 
-patches = patchify(input_image, (patch_size, patch_size, channels), step=patch_size)
-logging.debug(f"patches.shape: {patches.shape}")
+    input_image = Image.open(input_path)
+    input_image = np.asarray(input_image)
+    logging.debug(f"image.shape: {input_image.shape}")
 
-for i in range(patches.shape[0]):
-    for j in range(patches.shape[1]):
-        patch = patches[i, j, 0]
-        patch = Image.fromarray(patch)
+    patches = patchify(input_image, (patch_size, patch_size, channels), step=patch_size)
+    logging.debug(f"patches.shape: {patches.shape}")
 
-        output_file = f"{output_directory}/top_mosaic_09cm_area1_patch_{i}_{j}.tif"
-        patch.save(output_file)
-        # logging.info(f"Patch {output_file} saved.")
+    for i in range(patches.shape[0]):
+        for j in range(patches.shape[1]):
+            patch = patches[i, j, 0]
+            patch = Image.fromarray(patch)
+
+            output_file = f"{output_directory}/top_mosaic_09cm_area1_patch_{i}_{j}.tif"
+            patch.save(output_file)
+            logging.info(f"Patch {output_file} saved.")
+
+# TODO iterate over targets, too
