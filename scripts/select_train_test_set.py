189fd9e5afb62c185854f8b9ac28c4eedc67d8a8
Karel Ha <karel.ha@tum.de>
2023-02-21 20:00:23 +0100

    Generate filepaths from areas directly
    - instead of checking with `endswith`
    
    Signed-off-by: Karel Ha <karel.ha@tum.de>
    
    # Conflicts:
    #	scripts/pytorch_unfold_demo.py

--- scripts/select_train_test_set.py
+++ scripts/select_train_test_set.py
@@ -10,9 +10,11 @@ logging_level = logging.INFO
 logging.basicConfig(level=logging_level, format='[%(levelname)s] %(message)s')
 
 ground_truth_areas = [1, 3, 5, 7, 11, 13, 15, 17, 21, 23, 26, 28, 30, 32, 34, 37]
-allowed_areas = tuple([str(area) for area in ground_truth_areas])
 logging.debug(f"ground_truth_areas: {ground_truth_areas}")
 logging.info(f"len(ground_truth_areas): {len(ground_truth_areas)}")
+# allowed_areas = tuple([str(area) for area in ground_truth_areas])
+
+trunk_name = "top_mosaic_09cm_area"
 
 logging.debug(f"ls: {os.listdir()}")
 
@@ -21,13 +23,11 @@ logging.info(f"dir_top == {dir_top}")
 dir_ground_truth = Path('./data/vaihingen/ground_truth/')
 
 dir_train_img = f"../data/vaihingen/imgs/"
-for filename in os.listdir(dir_top):
-    name_without_extension = os.path.splitext(filename)[0]
-    logging.debug(f"name_without_extension == {name_without_extension}")
-    if name_without_extension.endswith(allowed_areas):
-        filepath = f"{dir_top}/{filename}"
-        logging.info(f"filepath ==  {filepath}")
-        shutil.copy(filepath, dir_train_img)
+for area in ground_truth_areas:
+    filename = f"{trunk_name}{area}.tif"
+    filepath = f"{dir_top}/{filename}"
+    logging.info(f"filepath ==  {filepath}")
+    shutil.copy(filepath, dir_train_img)
 
 dir_train_mask = Path('./data/vaihingen/masks/')
 
