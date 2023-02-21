e5df9c9b91a5abbadaea6ba911004a8d34dba85c
Karel Ha <karel.ha@tum.de>
2023-02-21 19:56:13 +0100

    Check name_without_extension to end with allowed_are
    Signed-off-by: Karel Ha <karel.ha@tum.de>
    
    # Conflicts:
    #	scripts/pytorch_unfold_demo.py

--- scripts/select_train_test_set.py
+++ scripts/select_train_test_set.py
@@ -2,13 +2,17 @@
 import logging
 import os
 from pathlib import Path
+import shutil
 
-# logging_level = logging.INFO
-logging_level = logging.DEBUG
+logging_level = logging.INFO
+# logging_level = logging.DEBUG
 # logging_level = logging.CRITICAL
 logging.basicConfig(level=logging_level, format='[%(levelname)s] %(message)s')
 
 ground_truth_areas = [1, 3, 5, 7, 11, 13, 15, 17, 21, 23, 26, 28, 30, 32, 34, 37]
+allowed_areas = tuple([str(area) for area in ground_truth_areas])
+logging.debug(f"ground_truth_areas: {ground_truth_areas}")
+logging.info(f"len(ground_truth_areas): {len(ground_truth_areas)}")
 
 logging.debug(f"ls: {os.listdir()}")
 
@@ -16,9 +20,15 @@ dir_top = "../data/vaihingen/top/"
 logging.info(f"dir_top == {dir_top}")
 dir_ground_truth = Path('./data/vaihingen/ground_truth/')
 
-dir_train_img = Path('./data/vaihingen/imgs/')
+dir_train_img = f"../data/vaihingen/imgs/"
 for filename in os.listdir(dir_top):
-    logging.debug(f"filename: {filename}")
+    name_without_extension = os.path.splitext(filename)[0]
+    logging.debug(f"name_without_extension == {name_without_extension}")
+    if name_without_extension.endswith(allowed_areas):
+        filepath = f"{dir_top}/{filename}"
+        logging.info(f"filepath ==  {filepath}")
+        shutil.copy(filepath, dir_train_img)
+
 dir_train_mask = Path('./data/vaihingen/masks/')
 
 dir_test_img = Path('./data/vaihingen/testset/imgs/')
