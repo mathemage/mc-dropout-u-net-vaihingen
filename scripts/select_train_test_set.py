b9b7cd21be8b359e100574e9f09cfa8b0adce2ef
Karel Ha <karel.ha@tum.de>
2023-02-22 17:24:36 +0100

    Use string for `dir_ground_truth` instead of Path
    Signed-off-by: Karel Ha <karel.ha@tum.de>

--- scripts/select_train_test_set.py
+++ scripts/select_train_test_set.py
@@ -12,15 +12,16 @@ logging.basicConfig(level=logging_level, format='[%(levelname)s] %(message)s')
 ground_truth_areas = [1, 3, 5, 7, 11, 13, 15, 17, 21, 23, 26, 28, 30, 32, 34, 37]
 logging.debug(f"ground_truth_areas: {ground_truth_areas}")
 logging.info(f"len(ground_truth_areas): {len(ground_truth_areas)}")
-# allowed_areas = tuple([str(area) for area in ground_truth_areas])
 
 trunk_name = "top_mosaic_09cm_area"
 
 logging.debug(f"ls: {os.listdir()}")
 
 dir_top = "../data/vaihingen/top/"
-logging.info(f"dir_top == {dir_top}")
-dir_ground_truth = Path('./data/vaihingen/ground_truth/')
+logging.info(f"dir_top == {dir_top}:\n{' '.join(os.listdir(dir_top))}")
+dir_ground_truth = "../data/vaihingen/ground_truth/"
+logging.info(f"dir_ground_truth == {dir_ground_truth}:\n{' '.join(os.listdir(dir_top))}")
+exit(1)
 
 dir_train_img = f"../data/vaihingen/imgs/"
 for area in ground_truth_areas:
