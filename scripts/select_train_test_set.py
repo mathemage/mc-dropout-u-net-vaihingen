08d1d737630428d7077a64d69dfc38935ad14e56
Karel Ha <karel.ha@tum.de>
2023-02-22 17:44:59 +0100

    Copy ground-truth areas to dir_train_img
    Signed-off-by: Karel Ha <karel.ha@tum.de>

--- scripts/select_train_test_set.py
+++ scripts/select_train_test_set.py
@@ -21,25 +21,23 @@ test_areas = [2, 4, 6, 8, 10, 12, 14, 16, 20, 22, 24, 27, 29, 31, 33, 35, 38]
 logging.info(f"test_areas: {test_areas}")
 logging.info(f"len(test_areas): {len(test_areas)}")
 
-# exit(1)
-
 dir_top = "../data/vaihingen/top/"
 logging.info(f"dir_top == {dir_top}:\n{' '.join(os.listdir(dir_top))}")
 dir_ground_truth = "../data/vaihingen/ground_truth/"
 logging.info(f"dir_ground_truth == {dir_ground_truth}:\n{' '.join(os.listdir(dir_top))}")
 
-dir_size = min(len(os.listdir(dir_top)), len(os.listdir(dir_ground_truth)))
-all_areas = range(1, dir_size + 1)
-logging.info(f"Total of {dir_size} areas.")
-
-dir_train_img = f"../data/vaihingen/imgs/"
-for area in all_areas:
+dir_train_img = "../data/vaihingen/trainset/imgs/"
+dir_train_mask = "../data/vaihingen/trainset/masks/"
+for area in ground_truth_areas:
     filename = f"{trunk_name}{area}.tif"
     filepath = f"{dir_top}/{filename}"
-    logging.info(f"filepath ==  {filepath}")
-    # shutil.copy(filepath, dir_train_img)
+    logging.debug(f"filepath ==  {filepath}")
+    shutil.copy(filepath, dir_train_img)
 
-dir_train_mask = Path('./data/vaihingen/masks/')
+logging.info(f"dir_train_img == {dir_train_img}:\n{' '.join(os.listdir(dir_train_img))}")
+logging.info(f"dir_train_mask == {dir_train_mask}:\n{' '.join(os.listdir(dir_train_mask))}")
+
+# exit(1)
 
 dir_test_img = Path('./data/vaihingen/testset/imgs/')
 dir_test_mask = Path('./data/vaihingen/testset/masks/')
