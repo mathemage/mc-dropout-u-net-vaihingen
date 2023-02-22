1e6e23f09d7032bd70f0ca4b742d4155aebcdcfb
Karel Ha <karel.ha@tum.de>
2023-02-22 17:37:11 +0100

    Introduce variable `test_areas`
    Signed-off-by: Karel Ha <karel.ha@tum.de>

--- scripts/select_train_test_set.py
+++ scripts/select_train_test_set.py
@@ -4,18 +4,24 @@ import os
 from pathlib import Path
 import shutil
 
+trunk_name = "top_mosaic_09cm_area"
+
 logging_level = logging.INFO
 # logging_level = logging.DEBUG
 # logging_level = logging.CRITICAL
 logging.basicConfig(level=logging_level, format='[%(levelname)s] %(message)s')
 
+logging.debug(f"ls: {os.listdir()}")
+
 ground_truth_areas = [1, 3, 5, 7, 11, 13, 15, 17, 21, 23, 26, 28, 30, 32, 34, 37]
-logging.debug(f"ground_truth_areas: {ground_truth_areas}")
+logging.info(f"ground_truth_areas: {ground_truth_areas}")
 logging.info(f"len(ground_truth_areas): {len(ground_truth_areas)}")
 
-trunk_name = "top_mosaic_09cm_area"
+test_areas = [2, 4, 6, 8, 10, 12, 14, 16, 20, 22, 24, 27, 29, 31, 33, 35, 38]
+logging.info(f"test_areas: {test_areas}")
+logging.info(f"len(test_areas): {len(test_areas)}")
 
-logging.debug(f"ls: {os.listdir()}")
+# exit(1)
 
 dir_top = "../data/vaihingen/top/"
 logging.info(f"dir_top == {dir_top}:\n{' '.join(os.listdir(dir_top))}")
@@ -23,16 +29,15 @@ dir_ground_truth = "../data/vaihingen/ground_truth/"
 logging.info(f"dir_ground_truth == {dir_ground_truth}:\n{' '.join(os.listdir(dir_top))}")
 
 dir_size = min(len(os.listdir(dir_top)), len(os.listdir(dir_ground_truth)))
-all_areas = range(1, dir_size)
+all_areas = range(1, dir_size + 1)
 logging.info(f"Total of {dir_size} areas.")
-# exit(1)
 
 dir_train_img = f"../data/vaihingen/imgs/"
-for area in ground_truth_areas:
+for area in all_areas:
     filename = f"{trunk_name}{area}.tif"
     filepath = f"{dir_top}/{filename}"
     logging.info(f"filepath ==  {filepath}")
-    shutil.copy(filepath, dir_train_img)
+    # shutil.copy(filepath, dir_train_img)
 
 dir_train_mask = Path('./data/vaihingen/masks/')
 
