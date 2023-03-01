402d1b548dadbae582d992ac0b8c6b089d928b39
Karel Ha <karel.ha@tum.de>
2023-03-01 15:45:25 +0100

    Extract variable `result` to be returned in evaluate()
    Signed-off-by: Karel Ha <karel.ha@tum.de>

--- evaluate_on_vaihingen.py
+++ evaluate_on_vaihingen.py
@@ -32,13 +32,11 @@ def evaluate(net, dataloader, device):
             else:
                 mask_pred = F.one_hot(mask_pred.argmax(dim=1), net.n_classes).permute(0, 3, 1, 2).float()
                 # compute the Dice score, ignoring background
-                dice_score += multiclass_dice_coeff(mask_pred[:, 1:, ...], mask_true[:, 1:, ...], reduce_batch_first=False)
-
-           
+                dice_score += multiclass_dice_coeff(mask_pred[:, 1:, ...], mask_true[:, 1:, ...],
+                                                    reduce_batch_first=False)
 
     net.train()
 
     # Fixes a potential division by zero error
-    if num_val_batches == 0:
-        return dice_score
-    return dice_score / num_val_batches
+    result = dice_score if num_val_batches == 0 else dice_score / num_val_batches
+    return result, result
