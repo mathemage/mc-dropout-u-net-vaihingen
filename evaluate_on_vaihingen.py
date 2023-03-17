e8c29ad48da73d24a60b8768f725528d6495ed94
Karel Ha <karel.ha@tum.de>
2023-03-17 14:25:01 +0100

    Extract variable `mask_true_one_hot`
    Signed-off-by: Karel Ha <karel.ha@tum.de>

--- evaluate_on_vaihingen.py
+++ evaluate_on_vaihingen.py
@@ -20,7 +20,7 @@ def evaluate(net, dataloader, device):
         # move images and labels to correct device and type
         image = image.to(device=device, dtype=torch.float32)
         mask_true = mask_true.to(device=device, dtype=torch.long)
-        mask_true = F.one_hot(mask_true, net.n_classes).permute(0, 3, 1, 2).float()
+        mask_true_one_hot = F.one_hot(mask_true, net.n_classes).permute(0, 3, 1, 2).float()
 
         with torch.no_grad():
             # predict the mask
@@ -30,11 +30,11 @@ def evaluate(net, dataloader, device):
             if net.n_classes == 1:
                 mask_pred = (F.sigmoid(mask_pred) > 0.5).float()
                 # compute the Dice score
-                dice_score += dice_coeff(mask_pred, mask_true, reduce_batch_first=False)
+                dice_score += dice_coeff(mask_pred, mask_true_one_hot, reduce_batch_first=False)
             else:
                 mask_pred = F.one_hot(mask_pred.argmax(dim=1), net.n_classes).permute(0, 3, 1, 2).float()
                 # compute the Dice score, ignoring background
-                dice_score += multiclass_dice_coeff(mask_pred[:, 1:, ...], mask_true[:, 1:, ...],
+                dice_score += multiclass_dice_coeff(mask_pred[:, 1:, ...], mask_true_one_hot[:, 1:, ...],
                                                     reduce_batch_first=False)
 
             # loss = criterion(mask_pred, mask_true) \
